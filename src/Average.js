de9671d2fe9358da25d7ddff1a2a7519ea43bbd5
velykitty <jiyunee13@gmail.com>
2021-02-07 01:53:51 +0900

    useCallback

--- src/Average.js
+++ src/Average.js
@@ -1,4 +1,4 @@
-import React, {useState, useMemo} from 'react';
+import React, {useState, useMemo, useCallback} from 'react';
 
 const getAverage = numbers => {
     console.log('get average...');
@@ -11,15 +11,25 @@ const Average = () => {
     const [list, setList] = useState([]);
     const [number, setNumber] = useState('');
 
-    const onChange = e => {
+    const onChange = useCallback(e => {
         setNumber(e.target.value);
-    };
+    }, []) //컴포넌트가 처음 렌더링될 때만 함수 생성
 
-    const onInsert = e => {
+    const onInsert = useCallback(e => {
         const nextList = list.concat(parseInt(number));
         setList(nextList);
         setNumber('');
-    };
+    }, [number, list]) //number 혹은 list가 바뀌었을 때만 함수 생성
+
+    // const onChange = e => {
+    //     setNumber(e.target.value);
+    // };
+
+    // const onInsert = e => {
+    //     const nextList = list.concat(parseInt(number));
+    //     setList(nextList);
+    //     setNumber('');
+    // };
 
     const avg = useMemo(() => getAverage(list), [list]);
     //list 배열이 바뀔 때만 getAverage 함수 호출
